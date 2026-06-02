d0cc9e840ac8088036d9ee421fed71315ad2205a
GatewayJ <835269233@qq.com>
2026-06-02 19:42:50 +0800

    chore: restore createMessageRequest formatter spacing

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1764,6 +1764,7 @@ func (r createMessageRequest) toServiceRequest() (im.CreateMessageRequest, error
 	if roomID == "" {
 		return im.CreateMessageRequest{}, fmt.Errorf("room_id is required")
 	}
+
 	return im.CreateMessageRequest{
 		RoomID:    roomID,
 		SenderID:  r.SenderID,
