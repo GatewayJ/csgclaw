46110ea6abc6c4a5627c96ea90d0036a60839a80
GatewayJ <835269233@qq.com>
2026-08-31 10:21:43 +0800

    feat(tasks): add per-task kanban boards

--- web/app/src/pages/WorkspacePage/components/WorkspaceSidebar/WorkspaceTabPanels.tsx
+++ web/app/src/pages/WorkspacePage/components/WorkspaceSidebar/WorkspaceTabPanels.tsx
@@ -100,8 +100,6 @@ type WorkspaceTabPanelsProps = Pick<
   | "onSelectThread"
   | "onToggleWorkspaceGroup"
   | "onViewTaskDetails"
-  | "planningTaskID"
-  | "startingTaskID"
   | "t"
   | "taskCount"
   | "scheduledTaskCount"
